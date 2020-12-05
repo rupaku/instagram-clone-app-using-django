624752764bf62fc8716030fdd534d7a7540fee82
rupaku <rupakumari201@yahoo.com>
2020-12-05 17:51:13 +0530

    authentication

--- insta_clone/authentication/views.py
+++ insta_clone/authentication/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic import View
+from django.http import HttpResponse
+from authentication.forms import UserForm
 # Create your views here.
+# def signup_view(request):
+#     if request.method =='POST':
+#         pass
+#     id request.method == 'GET':
+#     pass
+#     return render(request,template_name)
+
+def home(request):
+    return HttpResponse('home')
+
+class SignUpView(View):
+    template_name='authentication/signup.html'
+    form_class = UserForm
+    def get(self,request,*args, **kwargs):
+        return render(request, self.template_name)
+    def post(self,request,*args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home_view')
+        return render(request,self.template_name)
--- insta_clone/user/models.py
+++ insta_clone/user/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # from django.contrib.auth.base_user import BaseUserManager
+from user.manager import CustomUserManager
 
 # Create your models here.
+# AbstractUser to change in exiting user
 class User(AbstractUser):
     picture = models.ImageField(upload_to='profile_pictures',null=False,blank=False)
+    full_name = models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS =[]
+    #remove pre defined fields
+    first_name = None
+    last_name= None
 
-    # objects = BaseUserManager()
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS = ['full_name',]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
